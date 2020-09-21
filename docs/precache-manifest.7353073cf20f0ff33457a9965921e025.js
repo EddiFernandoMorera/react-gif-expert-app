b833e033252fe7a3dc5eb71e7ac90984fd72c0f9
Eddi Fernando Morera Hernandez <efmorerah@gmail.com>
2020-09-21 11:52:53 -0500

    Folder docs and files

---
+++ docs/precache-manifest.7353073cf20f0ff33457a9965921e025.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed5d1750f63ea2971af840857f5a7de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbd56409b82123fa7b31",
+    "url": "/static/css/main.eabf3b7c.chunk.css"
+  },
+  {
+    "revision": "51e34cff51b285543b30",
+    "url": "/static/js/2.04409896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04409896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbd56409b82123fa7b31",
+    "url": "/static/js/main.cb6996cf.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
